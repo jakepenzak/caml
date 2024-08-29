4033481c24c9185009f68675b903d6edcae6de1d
jakepenzak <jakepzak@gmail.com>
2024-08-29 21:23:20 +0000

    infra(test docker)

--- docker-run.sh
+++ docker-run.sh
@@ -5,4 +5,4 @@ docker run --rm -it \
     -v /caml/.venv \
     -v ~/.ssh:/root/.ssh \
     -w /caml \
-    caml bash -c "chown -R root:root /root/.ssh && chmod 700 /root/.ssh && chmod 600 /root/.ssh/id_rsa && chmod 600 /root/.ssh/config && bash"
+    caml
