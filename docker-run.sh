9f6a48d9fb1b3dacdadcaf44e03212f245a0833c
jakepenzak <jakepzak@gmail.com>
2024-08-29 20:16:03 +0000

    infra(test docker)

--- docker-run.sh
+++ docker-run.sh
@@ -1,14 +1,8 @@
-if [ -s 1 ]; then
-    MOUNT_SSH="-v ~/.ssh:/root/.ssh"
-else
-    MOUNT_SSH=""
-fi
-
 docker build -t caml -f Dockerfile .
 
 docker run --rm -it \
     -v $(pwd):/caml \
     -v /caml/.venv \
-    $MOUNT_SSH \
+    -v ~/.ssh:/root/.ssh \
     -w /caml \
     caml
