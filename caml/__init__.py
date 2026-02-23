3e9e1199ece70f141e85bc393029413668d71c36
jakepenzak <jacob@pieniazek.me>
2026-02-23 16:57:52 -0500

    refactor: continue api refactor

--- caml/__init__.py
+++ caml/__init__.py
@@ -8,21 +8,6 @@ from sklearn.utils.validation import DataConversionWarning
 from caml._version import __version__
 from caml.automl import AutoCATE
 
-# Filtering some benign warnings
-warnings.filterwarnings(
-    "ignore", category=DataConversionWarning, message="A column-vector y was passed"
-)
-warnings.filterwarnings(
-    "ignore",
-    category=UserWarning,
-    message="X does not have valid feature names, but LGBM",
-)
-warnings.filterwarnings("ignore", module="lightgbm")
-warnings.filterwarnings(
-    "ignore",
-    message=".*force_all_finite.*",
-    category=FutureWarning,
-)
 plt.style.use("ggplot")
 
 __all__ = ["__version__", "AutoCATE"]
